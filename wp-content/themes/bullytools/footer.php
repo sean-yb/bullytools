7cb6387349f142078df076be32d8249e990325d0
Sean Gracy <sean@ybinteractive.com>
2014-06-24 16:07:17 -0400

    - added layerslider(unofficial), google maps, contact form 7 plugin - style changes to nav, homepage, footer

--- wp-content/themes/bullytools/footer.php
+++ wp-content/themes/bullytools/footer.php
@@ -1,29 +1,26 @@
-			<footer role="contentinfo">
-			
-				<div id="inner-footer" class="clearfix">
-		          <hr />
-		          <div id="widget-footer" class="clearfix row">
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
-		            <?php endif; ?>
-		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
-		            <?php endif; ?>
-		          </div>
-					
-					<nav class="clearfix">
-						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
-					</nav>
-					
-					<p class="pull-right"><a href="http://320press.com" id="credit320" title="By the dudes of 320press">320press</a></p>
-			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
-				
-				</div> <!-- end #inner-footer -->
-				
-			</footer> <!-- end footer -->
-		
 		</div> <!-- end #container -->
+			<footer role="contentinfo">
+				<div class="container">
+					<div id="inner-footer" class="clearfix">
+			          <hr />
+			          <div id="widget-footer" class="clearfix row">
+			            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
+			            <?php endif; ?>
+			            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer2') ) : ?>
+			            <?php endif; ?>
+			            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer3') ) : ?>
+			            <?php endif; ?>
+			          </div>
+						
+						<nav class="clearfix">
+							<?php //wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
+						</nav>
+				
+						<p class="attribution">&copy; <?php echo date('Y');?> <?php bloginfo('name'); ?></p>
+					
+					</div> <!-- end #inner-footer -->
+				</div>
+			</footer> <!-- end footer -->
 				
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
--- wp-content/themes/bullytools/page-homepage.php
+++ wp-content/themes/bullytools/page-homepage.php
@@ -5,56 +5,14 @@ Template Name: Homepage
 ?>
 
 <?php get_header(); ?>
-			
+
 			<div id="content" class="clearfix row">
-			
+				<?php layerslider(1) ?>			
 				<div id="main" class="col-sm-12 clearfix" role="main">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 					
-						<header>
-
-							<?php 
-								$post_thumbnail_id = get_post_thumbnail_id();
-								$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpbs-featured-home' );
-							?>
-
-							<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-				
-								<div class="page-header">
-									<h1><?php bloginfo('title'); ?><small><?php echo get_post_meta($post->ID, 'custom_tagline' , true);?></small></h1>
-								</div>				
-								
-							</div>
-						
-						</header>
-						
-						<section class="row post_content">
-						
-							<div class="col-sm-8">
-						
-								<?php the_content(); ?>
-								
-							</div>
-							
-							<?php //get_sidebar('sidebar2'); // sidebar 2 ?>
-													
-						</section> <!-- end article header -->
-						
-						<footer>
-			
-							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					<?php 
-						// No comments on homepage
-						//comments_template();
-					?>
 					
 					<?php endwhile; ?>	
 					
